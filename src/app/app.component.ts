8be1769c5c6b8b2d3674eeaf8d246914124ef6df
DESKTOP-FO3JK3B\dibur <di.burd@yandex.ru>
2021-02-05 11:08:28 +0300

    Добавлен сервис обработки http запросов

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ApiService } from './core/api.service';
+import {HttpParams} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-shop-front';
+
+  constructor(private api: ApiService) {
+  }
+
+  testMethod() {
+    // const path = 'test/repiter';
+    const path = 'test/post';
+    const params = {
+      data1: 'xyz',
+      data2: 'pqr'
+    };
+
+    // this.api.get(path, null).subscribe({
+    //   next: (data: any) => console.log(),
+    //   error: err => console.log('err'),
+    //   complete: () => console.log('Compilation')
+    //   });
+    let p: HttpParams = new HttpParams({ fromString: 'param=param' });
+    this.api.post(path, params, p).subscribe({
+      next: (data: any) => console.log(data),
+      error: err => console.log('err'),
+      complete: () => console.log('Compilation')
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+/* Add Bootstrap */
+// import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { APP_PROVIDERS } from './app.providers';
+import { APP_IMPORTS } from './app.imports';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    APP_IMPORTS,
+
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    // TooltipModule.forRoot()
   ],
-  providers: [],
+  providers: [APP_PROVIDERS],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
